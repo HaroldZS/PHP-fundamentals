4ac78ad5d0db9b288f2e439f350e4ba7fe42e22e
haroldZS <haroldzs1024@gmail.com>
2023-08-15 12:31:20 -0400

    first commit

---
+++ print-strings.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = "Harold";
+$lastname = "Zurita";
+
+echo "My name is " . $name . " " . $lastname . "<br>";
+
+echo "My name is $name $lastname <br>";
+
+echo "4 x 5 = " . (4 * 5) . "<br>";
