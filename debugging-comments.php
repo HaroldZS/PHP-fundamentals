6463ec3dcc31b988335c4bbc37dddf775affe48a
haroldZS <haroldzs1024@gmail.com>
2023-08-15 12:45:11 -0400

    Debugging and comments

---
+++ debugging-comments.php
@@ -0,0 +1,21 @@
+<?php
+
+$people = [
+    "Harold" => 25,
+    "Zurita" => 15,
+    "Simon" => 65
+];
+
+var_dump($people);
+
+echo "<br>";
+
+print_r($people);
+
+echo "<br>";
+
+// Comment
+
+# More comments
+
+/* New comment */
