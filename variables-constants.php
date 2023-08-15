a3e5c97e0f64d5d49da7a2008893b5014f74ea50
haroldZS <haroldzs1024@gmail.com>
2023-08-15 14:49:01 -0400

    Variables and constants

---
+++ variables-constants.php
@@ -0,0 +1,16 @@
+<?php
+
+// Variables
+$number_1 = 8;
+$number_2 = 7;
+$string = "Harold";
+
+// Constants
+define("PI", 3.14);
+const NEW_CONSTANT = 29;
+
+echo $number_1 + $number_2;
+echo "<br>";
+echo PI;
+echo "<br>";
+echo NEW_CONSTANT;
