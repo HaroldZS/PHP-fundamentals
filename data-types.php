8d5fac60cad789507cce8daecf13802215fc4c12
haroldZS <haroldzs1024@gmail.com>
2023-08-16 00:24:29 -0400

    Data types

---
+++ data-types.php
@@ -0,0 +1,31 @@
+<?php
+
+// Integer
+$num = 12421421;
+
+var_dump($num);
+echo "<br>";
+
+// String
+$str = "25";
+$combination = $str + 1;
+
+var_dump($str);
+echo "<br>";
+
+var_dump($combination);
+echo "<br>";
+
+// Float
+$n = 10;
+$n = $n + 0.5;
+
+var_dump($n);
+echo "<br>";
+
+// Tests
+$potatoes = "10 potatoes";
+$potatoes_num = $potatoes + 5;
+
+echo $potatoes_num;
+echo "<br>";
