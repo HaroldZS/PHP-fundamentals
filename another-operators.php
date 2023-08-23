667d44ed8b605cd5569dc9fabee2e9e9917d84fb
haroldZS <haroldzs1024@gmail.com>
2023-08-23 00:14:15 -0400

    Another operators

---
+++ another-operators.php
@@ -0,0 +1,25 @@
+<?php
+
+// Asignation
+$age = ($harold_age = 19) + 6;
+echo $harold_age;
+echo "<br>";
+echo $age;
+echo "<br>";
+
+// += -= /= *=
+$counter = 1;
+$counter += 1;
+echo $counter;
+echo "<br>";
+$counter++;
+echo $counter;
+echo "<br>";
+++$counter;
+echo $counter;
+echo "<br>";
+
+// Concat
+$name = "Harold";
+$name .= " Zurita";
+echo $name;
